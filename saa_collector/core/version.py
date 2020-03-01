1a8aeeda449bda27db1291332b9ade16a10f93ca
Gao Rui <gaoruinan@163.com>
2020-03-01 22:19:07 +0800

    Fix packaging

--- saa_collector/core/version.py
+++ saa_collector/core/version.py
@@ -1,7 +1,5 @@
+from saa_collector.version import __version__
 
-from cement.utils.version import get_version as cement_get_version
 
-VERSION = (0, 0, 1, 'alpha', 0)
-
-def get_version(version=VERSION):
-    return cement_get_version(version)
+def get_version():
+    return __version__
--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
+import os
+
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
 
 from setuptools import setup, find_packages
-from saa_collector.core.version import get_version
 
-VERSION = get_version()
+version = {}
+with open(os.path.join(os.path.dirname(__file__), "saa_collector", "version.py")) as fp:
+    exec(fp.read(), version)
 
 f = open('README.md', 'r')
 LONG_DESCRIPTION = f.read()
 f.close()
 
+install_reqs = parse_requirements('requirements.txt', session='hack')
+reqs = [str(ir.req) for ir in install_reqs]
+
 setup(
     name='saa_collector',
-    version=VERSION,
+    version=version['__version__'],
     description='Collect SAA related data like stock basic data, financial data, etc.',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
@@ -19,12 +29,19 @@ setup(
     url='https://github.com/sinall/saa-collector/',
     license='unlicensed',
     packages=find_packages(exclude=['ez_setup', 'tests*']),
-    package_data={'saa_collector': ['templates/*']},
+    package_data={'saa_collector': ['templates/*', 'config/*']},
     include_package_data=True,
+    install_requires=reqs,
     entry_points={
         'console_scripts': [
             'saa_collector = saa_collector.main:main',
             'saa_collector_scheduler = saa_collector.scheduler:main',
         ],
     },
+    data_files=[
+        (os.path.join(os.path.expanduser('~'), '.saa_collector', 'config'), [
+            'config/saa_collector.yml.example',
+            'config/logging.conf.example',
+        ]),
+    ],
 )
